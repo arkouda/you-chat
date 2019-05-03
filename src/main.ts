7e3f1b44d075cc15e70e9495a497482a2bd162d6
Prabhtaj Singh Bhasin <burnwat3r@gmail.com>
2019-05-04 03:05:53 +0530

    Login final commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from "cors";
+// const server = require('http').createServer();
+// const io = require('socket.io')(server);
+
+
 async function bootstrap() {
+
+  // io.on('connection', function (client) {
+  //   console.log('New Connection');
+  // });
+  
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3001;
   app.use(cors());
